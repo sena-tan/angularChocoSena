9993afcc1125dd360a5790f704eb20d01470e4d7
sena-tan <sena.tan.bayraktar@hotmail.com>
2020-04-03 05:52:37 +0300

    angular footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 import { DishService } from './services/dish.service';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
